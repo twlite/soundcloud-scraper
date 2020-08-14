44520bede52b1a25db5a9d2e57ea3e7c6b27ea89
Androz2091 <androz2091@gmail.com>
2020-08-14 14:28:58 +0200

    :label: Add typings

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+declare module 'soundcloud-scraper' {
+
+    namespace SouncloudScraper {
+        interface SongData {
+            title: string,
+            author: string,
+            duration: number,
+            genre: string,
+            playCount: number,
+            commentsCount: number,
+            likeCount: number,
+            thumbnail: string
+        }
+
+        function validateURL(link: string): boolean;
+        function getSongInfo(link: string): SongData;
+    }
+
+    export = SouncloudScraper;
+
+}
