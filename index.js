84c159ffbca30fa76a7ab129a489d5c6bf2e4a89
Snowflake107 <devsnowflake107@gmail.com>
2020-10-31 20:16:43 +0545

    global export typedef

--- index.js
+++ index.js
@@ -4,14 +4,33 @@
 /////////////////////////////////////////
 
 const Util = require("./src/util/Util");
+const Client = require("./src/Client");
+const Constants = require("./src/constants/Constants");
+const keygen = Util.keygen;
+const Store = require("./src/store/Store");
+const StreamDownloader = require("./src/util/Downloader");
+const validateURL = Util.validateURL;
+const version = require("./package.json").version;
+
+/**
+ * @typedef {object} SoundCloud
+ * @property {Client} Client SoundCloud Client
+ * @property {Constants} Constants SoundCloud Utility constants
+ * @property {Util.keygen} keygen SoundCloud key generator
+ * @property {Store} Store SoundCloud Scraper cache
+ * @property {Util} Util SoundCloud Scraper Utility
+ * @property {StreamDownloader} StreamDownloader stream downloader
+ * @property {Util.validateURL} validateURL Validate soundcloud url
+ * @property {string} version Package version
+ */
 
 module.exports = {
-    Client: require("./src/Client"),
-    Constants: require("./src/constants/Constants"),
-    keygen: Util.keygen,
-    Store: require("./src/store/Store"),
-    StreamDownloader: require("./src/util/Downloader"),
-    Util: Util,
-    validateURL: Util.validateURL,
-    version: require("./package.json").version
+    Client,
+    Constants,
+    keygen,
+    Store,
+    StreamDownloader,
+    Util,
+    validateURL,
+    version
 };
